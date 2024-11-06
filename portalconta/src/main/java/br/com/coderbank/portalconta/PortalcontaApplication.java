c0fd118de429fce67177773abb0765ea9c5da7d7
willjp04 <sostenes655@gmail.com>
2024-11-05 21:03:31 -0300

    Commit projeto CoderBank "Conta"

---
+++ portalconta/src/main/java/br/com/coderbank/portalconta/PortalcontaApplication.java
@@ -0,0 +1,13 @@
+package br.com.coderbank.portalconta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalcontaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalcontaApplication.class, args);
+	}
+
+}
---
+++ portalconta/src/test/java/br/com/coderbank/portalconta/PortalcontaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coderbank.portalconta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalcontaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
